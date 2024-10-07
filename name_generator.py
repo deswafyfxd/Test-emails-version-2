0a459e95acf693002ed6b6b1394a6bd6450b4382
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-07 17:03:47 +0530

    Update name_generator.py

--- name_generator.py
+++ name_generator.py
@@ -1,40 +1,44 @@
 from faker import Faker
+import random
 
 fake = Faker()
 
-def generate_name(name_types):
+def generate_name(name_types, add_numbers):
     if name_types['personal_given_names']['indian']:
         fake = Faker('en_IN')
-        return fake.first_name().lower()
-    if name_types['personal_given_names']['western']:
+    elif name_types['personal_given_names']['western']:
         fake = Faker('en_US')
-        return fake.first_name().lower()
-    if name_types['personal_given_names']['japanese']:
+    elif name_types['personal_given_names']['japanese']:
         fake = Faker('en_US')
-        return fake.first_name().lower()
-    if name_types['personal_given_names']['chinese']:
+    elif name_types['personal_given_names']['chinese']:
         fake = Faker('en_US')
-        return fake.first_name().lower()
-    if name_types['personal_given_names']['other']:
+    elif name_types['personal_given_names']['other']:
         fake = Faker('en_US')
-        return fake.first_name().lower()
-    if name_types['surnames']:
+    elif name_types['surnames']:
         return fake.last_name().lower()
-    if name_types['nicknames']:
+    elif name_types['nicknames']:
         return fake.first_name().lower()
-    if name_types['brand_names']:
+    elif name_types['brand_names']:
         return fake.company().lower().replace(' ', '')
-    if name_types['place_names']:
+    elif name_types['place_names']:
         return fake.city().lower()
-    if name_types['pen_names']:
+    elif name_types['pen_names']:
         return f"{fake.first_name().lower()}_{fake.last_name().lower()}"
-    if name_types['stage_names']:
+    elif name_types['stage_names']:
         return f"{fake.first_name().lower()}_{fake.last_name().lower()}"
-    if name_types['usernames']:
+    elif name_types['usernames']:
         return fake.user_name().lower()
-    if name_types['scientific_names']:
+    elif name_types['scientific_names']:
         return f"species_{fake.word()}".lower()
-    return fake.first_name().lower()  # Default to personal names if all false
+    else:
+        name = fake.first_name().lower()  # Default to personal names if all false
+
+    if add_numbers['enabled']:
+        digits = add_numbers.get('digits', 2)
+        number = ''.join(random.choices('0123456789', k=digits))
+        name = f"{name}{number}"
+
+    return name
 
 def generate_dot_variations(username):
     variations = set()
