aa60c185e9cfa07e9c22558d8aa2f4ba69681ba7
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-07 19:38:00 +0530

    Update generate_emails.py

--- generate_emails.py
+++ generate_emails.py
@@ -65,7 +65,8 @@ def main():
     if control_config['notification']['txt_file_to_discord']:
         send_txt_to_discord(gmail_emails + outlook_emails, discord_webhook_url)
 
-    if control_config['github_action']['enabled']:
+    # Check if GitHub action is enabled before pushing
+    if control_config.get('github_action', {}).get('enabled', False):
         push_to_github()
 
 if __name__ == "__main__":
