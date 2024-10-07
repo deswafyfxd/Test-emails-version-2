f66b53d12b242de24ad58840f629ce67945c5350
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-07 18:10:14 +0530

    Update generate_emails.py

--- generate_emails.py
+++ generate_emails.py
@@ -2,6 +2,13 @@ import os
 from config_loader import load_config
 from email_generator import generate_emails, write_to_file
 from notification_sender import send_to_discord, send_txt_to_discord
+import subprocess
+
+def push_to_github(domain_folder):
+    commit_message = f"Add generated emails for {domain_folder}"
+    subprocess.run(["git", "add", domain_folder])
+    subprocess.run(["git", "commit", "-m", commit_message])
+    subprocess.run(["git", "push"])
 
 def main():
     control_config = load_config('config_control.yml')
@@ -42,10 +49,18 @@ def main():
     if control_config['gmail']['enabled']:
         gmail_emails = generate_emails(email_config['gmail'], name_types, add_numbers, gmail_total_count, gmail_plus_count, gmail_dot_variation_count, gmail_plus_dot_combination_count, "gmail.com", gmail_plus_enabled, gmail_dot_variation_enabled, gmail_plus_dot_combination_enabled)
         write_to_file("gmail_emails.txt", gmail_emails)
+        if control_config['github_action']['enabled']:
+            if not os.path.exists('gmail'):
+                os.makedirs('gmail')
+            write_to_file("gmail/generated_emails.txt", gmail_emails)
 
     if control_config['outlook']['enabled']:
         outlook_emails = generate_emails(email_config['outlook'], name_types, add_numbers, outlook_total_count, outlook_plus_count, outlook_dot_variation_count, outlook_plus_dot_combination_count, "outlook.com", outlook_plus_enabled, outlook_dot_variation_enabled, outlook_plus_dot_combination_enabled)
         write_to_file("outlook_emails.txt", outlook_emails)
+        if control_config['github_action']['enabled']:
+            if not os.path.exists('outlook'):
+                os.makedirs('outlook')
+            write_to_file("outlook/generated_emails.txt", outlook_emails)
 
     discord_webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
 
@@ -54,6 +69,10 @@ def main():
 
     if control_config['notification']['txt_file_to_discord']:
         send_txt_to_discord(gmail_emails + outlook_emails, discord_webhook_url)
+
+    if control_config['github_action']['enabled']:
+        push_to_github("gmail")
+        push_to_github("outlook")
 
 if __name__ == "__main__":
     main()
